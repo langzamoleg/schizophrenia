e7605f699607538c4998d571e2dc3120bd5f7b3b
Oleg Zamyatin <langzamoleg@yandex.ru>
2024-06-14 21:13:00 +0300

    bimbimbambam

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,4 @@
 <script>
-	export let name;
-
 	let button1text = "first request";
 	let button2text = "second request";
 
@@ -20,11 +18,46 @@
 			})
 		});
 	})
+
+	function handleSubmit() {
+		var files = document.forms['formName']['files'].files;
+		console.log(files);
+		console.log(document.forms['formName']['cardId'].value);
+		console.log(document.forms['formName']['authToken'].value);
+		var counter = 4;
+		Array.prototype.forEach.call(files, function (file) {
+			let formData = new FormData();
+			formData.append('file', file);
+			fetch("https://api.sellershands.ru/api/cards/card/"+document.forms['formName']['cardId'].value+"/photo?" + new URLSearchParams({ authToken: document.forms['formName']['authToken'].value, number: Math.abs(counter-5) }).toString(), {
+				method: "POST",
+				mode: "no-cors",
+				body: formData
+			}).then((res) => {
+				console.log(res);
+			})
+			counter--;
+		})
+		for (let i = 0; i < counter; i++) {
+			fetch("https://api.sellershands.ru/api/cards/card/"+document.forms['formName']['cardId'].value+"/photo?"+ new URLSearchParams({ authToken: document.forms['formName']['authToken'].value, number: Math.abs(counter-5) }).toString(), {
+				method: "DELETE"
+			}).then((res) => {
+				console.log(res);
+			})
+			counter--;
+		}
+		return false;
+	}
 </script>
 
 <main>
 	<button id='button1'>{button1text}</button>
 	<button id='button2'>{button2text}</button>
+	<form on:submit={handleSubmit} formName="formName" name="formName" onsubmit="return false">
+		<input type='text' placeholder="authToken" name="authToken">
+		<input type='text' placeholder="cardId" name="cardId">
+		<input type='file' multiple name="files">
+		<button>send</button>
+	</form>
 </main>
 
 <style>
@@ -33,13 +66,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
